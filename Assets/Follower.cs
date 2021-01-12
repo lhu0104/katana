59a6fecf4cec5a359fe66c787eefccd9c3084d37
lhu0104 <lhu0104@gmail.com>
2021-01-13 03:04:01 +0900

    게임 뷰

---
+++ Assets/Follower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    Vector3 to;
+    Vector3 diff;
+    private void Start() {
+        SetDiff();    
+    }
+    void SetDiff()
+    {
+        diff = this.transform.position - target.transform.position;
+    }
+    void LateUpdate()
+    {
+        to = diff + target.transform.position;
+        this.transform.position = Vector3.Lerp(to, this.transform.position, .1f);
+    }
+}
